a0391cd362ad112f9a104fc9a12c6a01daf29945
JackAnlauf <prentrepreneur29@gmail.com>
2024-08-26 14:10:35 -0500

    Hello World

---
+++ HelloWorldJackA.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//regular use of print() method
+
+print("Hello, World!")
+
+
+//fancy use of print() method
+let word1 = "Hello"
+let word2 = "World"
+
+print(word1,word2,separator: ", ", terminator: "!")
