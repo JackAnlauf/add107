22107f7417620d2d8e719e9e1c7cacc5ae9c1910
JackAnlauf <prentrepreneur29@gmail.com>
2024-09-04 13:02:52 -0500

    added '8 constants and variables'

--- HelloWorldJackA.playground/Contents.swift
+++ HelloWorldJackA.playground/Contents.swift
@@ -5,8 +5,18 @@ import UIKit
 print("Hello, World!")
 
 
+
 //fancy use of print() method
 let word1 = "Hello"
 let word2 = "World"
 
+
 print(word1,word2,separator: ", ", terminator: "!")
+
+
+
+
+
+
+
+
